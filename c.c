8a741ab2a385297a25c1e014202529589c8fd7ed
Debangshu Kumar Mondal <debangshukumarmondal233217@gmail.com>
2026-09-25 17:26:15 +0530

    Add files via upload

--- c.c
+++ c.c
@@ -197,15 +197,44 @@
 
 //program a c program display odd numbers from 1 to n
 
+//#include<stdio.h>
+//int main(){
+//	int num = 1,i;
+//	printf("Enter a number: ");
+//	scanf("%d",&i);
+//	while(num<=i)
+//	{
+//		if(num%2!=0)
+//		printf("\nodd number is: %d",num);
+//		num++;
+//	}
+//}
+
+//write  a C programe to find the sum of the following series1!+3!+5!.....upto n numbers*/
+//1! =1
+//3!=3*2*1=6
+//5!=5*4*3*1=120
+//upto n !
 #include<stdio.h>
-int main(){
-	int num = 1,i;
-	printf("Enter a number: ");
-	scanf("%d",&i);
-	while(num<=i)
-	{
-		if(num%2!=0)
-		printf("\nodd number is: %d",num);
-		num++;
+int main()
+{
+	int i=1, c=1,a=1,n;
+	long int fact,sum=0;
+	printf("enter the number of terms:");
+	scanf("%d",&n);
+	
+	while(c<=n){
+		i=1;
+		fact=1;
+		while(i<=a){
+			fact=fact*i;
+			i++;
+		}
+		sum=sum+fact;
+		c++;
+		a=a+2;
 	}
+	printf("sum of the numbers=%d",sum);
+	return 0;
 }
+   
